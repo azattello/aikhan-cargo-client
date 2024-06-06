468536c913be3cd0f049d531f136a71cd3925d98
Abdykali Azat <azat.abdykalli@gmail.com>
2024-06-06 18:36:37 +0500

    aikhan client

---
+++ src/config.js
@@ -0,0 +1,11 @@
+// config.js
+const config = {
+    // apiUrl: 'http://localhost:3001',
+    apiUrl: 'https://aikhan-cargo.kz:3001',
+    
+    nameCargo: 'Aikhan-Cargo'
+  };
+  
+  
+export default config;
+  
